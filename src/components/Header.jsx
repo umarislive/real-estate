4282c958375fea052f6f1faffb778673ea1c2602
Umar Farooq <umarisonline007@gmail.com>
2025-10-31 08:22:41 +0500

    Real Estate Landing Page

---
+++ src/components/Header.jsx
@@ -0,0 +1,36 @@
+import Navbar from "./Navbar";
+import { motion } from "motion/react";
+import { assets } from "../assets/assets";
+
+const Header = () => {
+  return (
+    <div
+      className="min-h-screen mb-4 bg-cover bg-center flex items-center w-full overflow-hidden"
+      style={{ backgroundImage: `url(${assets.header_img})` }}
+      id="Header"
+    >
+      <Navbar />
+      <motion.div
+        initial={{opacity: 0, y:100}}
+        transition={{duration: 1.5}}
+        whileInView={{opacity: 1, y: 0}}
+        viewport={{once: true}}
+        className="container text-center mx-auto py-4 px-6 md:px-20 lg:px-32 text-white"
+      >
+        <h2 className="text-5xl font-semibold sm:text-6xl md:text-[82px] inline-block max-w-3xl pt-20">
+          Explore homes that fit your dreams
+        </h2>
+        <div className="space-x-6 mt-16">
+          <a href="#Projects" className="border border-white px-8 py-3 rounded">
+            Projects
+          </a>
+          <a href="#Contact" className="bg-blue-500 px-8 py-3 rounded">
+            Contact Us
+          </a>
+        </div>
+      </motion.div>
+    </div>
+  );
+};
+
+export default Header;
